de519740ecd52398f5c1ed51b9d5e103535f2809
Lamont Granquist <lamont@scriptkiddie.org>
2016-03-11 15:12:51 -0800

    bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "cookbooks@chef.io"
 license          "Apache 2.0"
 description      "Configures the hostname on a node"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
-version          "0.2.0"
+version          "0.3.0"
 source_url       "https://github.com/chef-cookbooks/chef-cookbooks" if respond_to?(:source_url)
 issues_url       "https://github.com/chef-cookbooks/chef-cookbooks/issues" if respond_to?(:issues_url)
 
