f8dd512e6e17eafc1e30e44b3ab553519c31294e
Lamont Granquist <lamont@scriptkiddie.org>
2016-03-21 15:57:11 -0700

    fix issues urls

--- metadata.rb
+++ metadata.rb
@@ -5,8 +5,8 @@ license          "Apache 2.0"
 description      "Configures the hostname on a node"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
 version          "0.3.0"
-source_url       "https://github.com/chef-cookbooks/chef-cookbooks" if respond_to?(:source_url)
-issues_url       "https://github.com/chef-cookbooks/chef-cookbooks/issues" if respond_to?(:issues_url)
+source_url       "https://github.com/lamont-cookbooks/chef_hostname" if respond_to?(:source_url)
+issues_url       "https://github.com/lamont-cookbooks/chef_hostname/issues" if respond_to?(:issues_url)
 
 depends "compat_resource"
 
